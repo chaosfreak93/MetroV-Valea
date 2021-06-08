9f2e6098398d50f8d47aa971bbfd6ca27876608d
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-06-08 22:52:08 +0200

    Fixed SaltyChat and the related TeamSpeak freezes/crashes

--- script-a-master/Server/resources/SaltyClient/Client/Models/SaltyChat/GameInstance.js
+++ script-a-master/Server/resources/SaltyClient/Client/Models/SaltyChat/GameInstance.js
@@ -1,5 +1,6 @@
 export class GameInstance {
-    constructor(serverUniqueIdentifier, name, channelId, channelPassword, soundPack, swissChannels, sendTalkStates, sendRadioTrafficStates, ultraShortRangeDistance, shortRangeDistance, longRangeDistance) {
+    constructor(serverUniqueIdentifier, name, channelId, channelPassword, soundPack, swissChannels, sendTalkStates, sendRadioTrafficStates, rRangeUltraShort, rRangeShort, rRangeLong) {
+        this.versionInfo = "1.0.7P2";
         this.serverUniqueIdentifier = serverUniqueIdentifier;
         this.name = name;
         this.channelId = channelId;
@@ -8,8 +9,8 @@ export class GameInstance {
         this.swissChannelIds = swissChannels;
         this.sendTalkStates = sendTalkStates;
         this.sendRadioTrafficStates = sendRadioTrafficStates;
-        this.ultraShortRangeDistance = ultraShortRangeDistance;
-        this.shortRangeDistance = shortRangeDistance;
-        this.longRangeDistance = longRangeDistance;
+        this.ultraShortRangeDistance = rRangeUltraShort;
+        this.shortRangeDistance = rRangeShort;
+        this.longRangeDistance = rRangeLong;
     }
 }
