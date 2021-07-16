1ab72b43ab14d2923244604bdcb0b757f88a02aa
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-07-16 22:19:04 +0200

    Bigger Casino Teleport Points

--- script-a-master/Altv-Roleplay/AltV.Roleplay/Handler/KeyHandler.cs
+++ script-a-master/Altv-Roleplay/AltV.Roleplay/Handler/KeyHandler.cs
@@ -157,7 +157,7 @@ namespace Altv_Roleplay.Handler
                 Position casinoEntrancePos = new Position(935.94727f, 47.20879f, 80.293017578125f);
                 Position casinoExitPos = new Position(1089.7847f, 206.09671f, -49.80439453125f);
                 
-                bool casinoEntrance = player.Position.IsInRange(casinoEntrancePos, 2f);
+                bool casinoEntrance = player.Position.IsInRange(casinoEntrancePos, 3f);
 
                 if (casinoEntrance && !player.IsInVehicle) {
                     player.Emit("Casino:Enter");
@@ -165,7 +165,7 @@ namespace Altv_Roleplay.Handler
                     return;
                 }
                 
-                bool casinoExit = player.Position.IsInRange(casinoExitPos, 2f);
+                bool casinoExit = player.Position.IsInRange(casinoExitPos, 3f);
 
                 if (casinoExit && !player.IsInVehicle) {
                     player.Emit("Casino:Leave");
