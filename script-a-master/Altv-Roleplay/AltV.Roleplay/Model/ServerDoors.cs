de08ce9f50c6c70c00bbc037f6fd49cf2f4416b6
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-07-22 12:47:16 +0200

    AAAAAAAAAAAAA

--- script-a-master/Altv-Roleplay/AltV.Roleplay/Model/ServerDoors.cs
+++ script-a-master/Altv-Roleplay/AltV.Roleplay/Model/ServerDoors.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using AltV.Net.Elements.Entities;
-using AltV.Net.Interactions;
 using Altv_Roleplay.models;
 
 namespace Altv_Roleplay.Model
@@ -8,7 +7,7 @@ namespace Altv_Roleplay.Model
     internal class ServerDoors
     {
         public static List<Server_Doors> ServerDoors_ = new();
-        public static List<Interaction> ServerDoorsColshapes_ = new();
-        public static List<Interaction> ServerDoorsLockColshapes_ = new();
+        public static List<IColShape> ServerDoorsColshapes_ = new();
+        public static List<IColShape> ServerDoorsLockColshapes_ = new();
     }
 }
