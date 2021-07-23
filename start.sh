7558ff3115c9e66c780dae95dfc776662557d404
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-07-23 16:08:12 +0200

    Load Map Submodule

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/MetroV/script-a-master/Server/
-screen -dmS "altv" ./altv-server --extra-res-folder cars/
+screen -dmS "altv" ./altv-server --extra-res-folder cars/ --extra-res-folder maps/
