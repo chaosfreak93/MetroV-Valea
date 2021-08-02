736f22d2625e9a403ad5baa39b1ec84c10142a5c
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-08-02 22:40:11 +0200

    Other Sit Animation

--- script-a-master/Server/resources/roleplay/client/hud.js
+++ script-a-master/Server/resources/roleplay/client/hud.js
@@ -1564,7 +1564,8 @@ function InterActionMenuDoActionAnimationMenu(action) {
         } else if (action == "inspect") {
             playAnimation("random@train_tracks", "idle_e", 1, 300000)
         } else if (action == "sit") {
-            playAnimation("timetable@ron@ig_5_p3", "ig_5_p3_base", 1, 300000)
+            //playAnimation("timetable@ron@ig_5_p3", "ig_5_p3_base", 1, 300000)
+            playAnimation("amb@prop_human_seat_chair@male@generic@base", "base", 1, 300000)
         } else if (action == "lean4") {
             playAnimation("amb@world_human_leaning@male@wall@back@foot_up@idle_a", "idle_a", 1, 300000)
         } else if (action == "mechanic") {
