19e3bc23f94d1995cca144931f17d159a459c27d
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-08-03 11:36:05 +0200

    Fix Admin Menü Pos

--- script-a-master/Server/resources/roleplay/client/adminmenu.js
+++ script-a-master/Server/resources/roleplay/client/adminmenu.js
@@ -21,7 +21,7 @@ let playerblips_interval = null;
 
 alt.onServer("Client:WritePosAndRot", (pos, rot) => {
     alt.log(`-----------------------------------------------`);
-    alt.log(`Position: ${pos.x} , ${pos.y} , ${pos.z + 0.95}`);
+    alt.log(`Position: ${pos.x} , ${pos.y} , ${pos.z - 0.95}`);
     alt.log(`Rotation: ${rot.x} , ${rot.y} , ${rot.z}`);
 });
 
