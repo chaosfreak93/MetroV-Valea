a8375d5505800ad2f2f04eec768883fd2c7a8d3c
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-08-06 12:06:45 +0200

    Update .gitignore

---
+++ script-a-master/Server/resources/roleplay/client/blackout.js
@@ -0,0 +1,39 @@
+/// <reference types="@altv/types-client" />
+/// <reference types="@altv/types-natives" />
+import * as alt from 'alt-client';
+import * as game from 'natives';
+
+let isBlackedOut = false;
+let oldBodyDamage = 0;
+
+function blackout() {
+    if (!isBlackedOut) {
+        isBlackedOut = true;
+        game.doScreenFadeOut(100);
+        alt.setTimeout(() => {
+            game.doScreenFadeIn(250);
+            isBlackedOut = false;
+        }, 2000);
+    }
+}
+
+alt.setInterval(() => {
+    let vehicle = game.getVehiclePedIsIn(alt.Player.local.scriptID, false);
+    if (game.doesEntityExist(vehicle)) {
+        let currentDamage = game.getVehicleBodyHealth(vehicle);
+        if (currentDamage != oldBodyDamage) {
+            if (!isBlackedOut && (currentDamage < oldBodyDamage) && ((oldBodyDamage - currentDamage) >= 25)) {
+                blackout();
+            }
+            oldBodyDamage = currentDamage;
+        }
+    }
+
+    if (isBlackedOut) {
+        game.disableControlAction(0, 71, true);
+        game.disableControlAction(0, 72, true);
+        game.disableControlAction(0, 63, true);
+        game.disableControlAction(0, 64, true);
+        game.disableControlAction(0, 75, true);
+    }
+}, 100);
