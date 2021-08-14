801478c12bad6cc52a5bfd358f3ffd3685b62cda
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-08-14 16:26:39 +0200

    Rework Crouch

--- script-a-master/Server/resources/roleplay/client/crouch.js
+++ script-a-master/Server/resources/roleplay/client/crouch.js
@@ -3,23 +3,40 @@ import * as native from 'natives';
 import { loadClipsetAsync } from './utilities';
 const player = alt.Player.local;
 let enabled = false;
-const movementClipSet = "move_ped_crouched";
+const movementClipSet1 = "MOVE_M@TOUGH_GUY@";
+const movementClipSet2 = "move_ped_crouched";
 const strafeClipSet = "move_ped_crouched_strafing";
 const clipSetSwitchTime = 0.25;
+let interval = null;
 class Crouch {
     static async start() {
         if (enabled) return;
         enabled = true;
-        await loadClipsetAsync(movementClipSet);
+        await loadClipsetAsync(movementClipSet1);
+        await loadClipsetAsync(movementClipSet2);
         await loadClipsetAsync(strafeClipSet);
-        native.setPedMovementClipset(player.scriptID, movementClipSet, clipSetSwitchTime);
-        native.setPedStrafeClipset(player.scriptID, strafeClipSet);
+        native.setPedUsingActionMode(player.scriptID, false, -1, "DEFAULT_ACTION");
+        interval = alt.setInterval(Crouch.checkCrouch, 200);
     }
     static stop() {
         if (!enabled) return;
         enabled = false;
-        native.resetPedMovementClipset(player.scriptID, clipSetSwitchTime);
-        native.resetPedStrafeClipset(player.scriptID);
+        native.setPedUsingActionMode(player.scriptID, false, -1, "DEFAULT_ACTION");
+        native.setPedMovementClipset(player.scriptID, movementClipSet1, clipSetSwitchTime);
+        alt.clearInterval(interval);
+        alt.setTimeout(()=>{
+            native.resetPedMovementClipset(player.scriptID, 0);
+            native.resetPedStrafeClipset(player.scriptID);
+        }, 200);
+    }
+    static checkCrouch() {
+        native.setPedMovementClipset(player.scriptID, movementClipSet2, clipSetSwitchTime);
+        native.disableControlAction(0, 22, true); //Jump
+        native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
+        if (native.isAimCamActive()) {
+            native.setPedStrafeClipset(player.scriptID, strafeClipSet);
+            native.setPedUsingActionMode(player.scriptID, false, -1, 'DEFAULT_ACTION');
+        }
     }
 }
 export { Crouch as default };
