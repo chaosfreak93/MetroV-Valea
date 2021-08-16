e541827d641625ac86703f1ddec1206083524db4
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-08-16 17:52:34 +0200

    Change DB

--- script-a-master/C#/AltV.Roleplay/Utils/Constants.cs
+++ script-a-master/C#/AltV.Roleplay/Utils/Constants.cs
@@ -6,7 +6,7 @@ namespace Altv_Roleplay.Utils
     {
         public static class DatabaseConfig
         {
-            public static string Host = "5.181.151.136";
+            public static string Host = "127.0.0.1";
             public static string User = "altv";
             public static string Password = "qS*qD7tc@cv#aJtu";
             public static string Port = "3306";
