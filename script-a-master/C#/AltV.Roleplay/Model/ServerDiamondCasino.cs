c355e56d78c41b300d70b2f5dccdee878ad599b8
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-08-22 16:05:46 +0200

    Anonymous Calls

---
+++ script-a-master/C#/AltV.Roleplay/Model/ServerDiamondCasino.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AltV.Net;
+using Altv_Roleplay.models;
+
+namespace Altv_Roleplay.Model
+{
+    public class ServerDiamondCasino : IScript
+    {
+        public static List<Server_Diamond_Casino> DiamondCasino = new();
+
+        public static void setPodiumVehicle(string podiumVehicle) {
+            using (var db = new gtaContext()) {
+                DiamondCasino = new List<Server_Diamond_Casino>(db.Server_Diamond_Casino);
+
+                foreach (var casino in DiamondCasino) {
+                    casino.podiumVehicle = podiumVehicle;
+                    Alt.SetSyncedMetaData("podiumVehicle", podiumVehicle);
+                    db.Server_Diamond_Casino.Update(casino);
+                    db.SaveChanges();
+                }
+            }
+        }
+        
+        public static string getPodiumVehicle() {
+            using (var db = new gtaContext()) {
+                DiamondCasino = new List<Server_Diamond_Casino>(db.Server_Diamond_Casino);
+
+                return DiamondCasino.First().podiumVehicle;
+            }
+        }
+    }
+}
